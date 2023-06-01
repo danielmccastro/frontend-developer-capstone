29379d52782b8b2229a7d522aa52293ba148ef95
Daniel Castro <120190089+danielmccastro@users.noreply.github.com>
2023-06-01 15:30:05 -0300

    Semantic structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return (
+            <footer>
+                <img src="" alt="" />
+                <div><p>TEXTO</p></div>
+                <div><p>TEXTO</p></div>
+            </footer>
+    )
+}
+
+export default Footer;
