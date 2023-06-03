5a971351cf0b95caa984443f47b01ddb2907a0d5
Daniel Castro <120190089+danielmccastro@users.noreply.github.com>
2023-06-03 10:03:47 -0300

    Installed the bootstrap dependency and updated the header with bootstrap classes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import Layout from "./components/Layout"
 
 function App() {
   return (
     <>
-    <Header />
-    <Main />
-    <Footer />
+    <Layout />
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,64 @@
 import React from "react";
-import LittleLemonLogo from "../assets/Logo.png"
+import LittleLemonLogo from "../assets/Logo.png";
 
 function Header() {
   return (
-    <header className="header">
-      <img className="header-logo" src={LittleLemonLogo} alt="Little Lemon Logo" />
-      <nav className="navbar">
-        <ul>
-          <li>
-            <a href="">Home</a>
-          </li>
-          <li>
-            <a href="">About</a>
-          </li>
-          <li>
-            <a href="">Menu</a>
-          </li>
-          <li>
-            <a href="">Reservations</a>
-          </li>
-          <li>
-            <a href="">Order Online</a>
-          </li>
-          <li>
-            <a href="">Login</a>
-          </li>
-        </ul>
+    <header>
+      <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img
+              className="header-logo"
+              src={LittleLemonLogo}
+              alt="Little Lemon Logo"
+            />
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Menu
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Reservations
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Order Online
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Login
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
     </header>
   );
