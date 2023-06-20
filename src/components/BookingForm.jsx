f43fdbc4acbeb9ce0f572dc75e7635115703fa64
Daniel Castro <120190089+danielmccastro@users.noreply.github.com>
2023-06-20 09:57:20 -0300

    Update form and confirmation page

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
-import { useFormik } from "formik";
-import * as Yup from 'yup';
 
 function BookingForm({ availableTimes, dispatch, submitForm }) {
-
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -13,16 +10,16 @@ function BookingForm({ availableTimes, dispatch, submitForm }) {
     occasion: "Select an Option",
   });
 
-  const handleFormChange = (event) => {
-    const { name, value } = event.target;
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
     setFormData((prevFormData) => ({
       ...prevFormData,
       [name]: value,
     }));
   };
 
-  const handleDateChange = async (event) => {
-    const { name, value } = event.target;
+  const handleDateChange = async (e) => {
+    const { name, value } = e.target;
 
     setFormData((prevFormData) => ({
       ...prevFormData,
@@ -44,106 +41,109 @@ function BookingForm({ availableTimes, dispatch, submitForm }) {
   return (
     <main className="container">
       <div className="row">
-        <h1 className="mt-4 text-h1">Reserve a table</h1>
-        <form className="mb-4 text-karla" onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label htmlFor="res-name" className="form-label">
-              Name
-            </label>
-            <input
-              type="text"
-              id="res-name"
-              name="name"
-              className="form-control"
-              value={formData.name}
-              onChange={handleFormChange}
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="res-email" className="form-label">
-              Email
-            </label>
-            <input
-              type="email"
-              id="res-email"
-              name="email"
-              className="form-control"
-              value={formData.email}
-              onChange={handleFormChange}
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="res-date" className="form-label">
-              Choose date
-            </label>
-            <input
-              type="date"
-              id="res-date"
-              name="date"
-              className="form-control"
-              value={formData.date}
-              onChange={handleDateChange}
-              min={currentDate}
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="res-time">Choose time</label>
-            <select
-              id="res-time"
-              name="time"
-              value={formData.availableTimes}
-              onChange={handleFormChange}
-              required
-            >
-              {options}
-            </select>
-          </div>
-          <div className="mb-3">
-            <label htmlFor="guests" className="form-label">
-              Number of guests
-            </label>
-            <input
-              type="number"
-              placeholder="1"
-              min="1"
-              max="10"
-              id="guests"
-              name="guests"
-              className="form-control"
-              value={formData.guests}
-              onChange={handleFormChange}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="occasion" className="form-label">
-              Occasion
-            </label>
-            <select
-              id="occasion"
-              name="occasion"
-              className="form-select"
-              key={formData.occasion}
-              value={formData.occasion}
-              onChange={handleFormChange}
-              required
-            >
-              <option value="">Select an Option</option>
-              <option>Birthday</option>
-              <option>Anniversary</option>
-            </select>
-          </div>
-          <div>
-            <input
-              aria-label="On Click"
-              type="submit"
-              value={"Make Your Reservation"}
-              className="btn-reserve"
-            />
-          </div>
-        </form>
+        <div className="col-4 mx-auto">
+          <h1 className="mt-4 text-h1">Reserve a table</h1>
+          <form className="mb-4 text-karla" onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="res-name" className="form-label">
+                Name
+              </label>
+              <input
+                type="text"
+                id="res-name"
+                name="name"
+                className="form-control bg-gray"
+                value={formData.name}
+                onChange={handleFormChange}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="res-email" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                id="res-email"
+                name="email"
+                className="form-control bg-gray"
+                value={formData.email}
+                onChange={handleFormChange}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="res-date" className="form-label">
+                Choose date
+              </label>
+              <input
+                type="date"
+                id="res-date"
+                name="date"
+                className="form-control bg-gray"
+                value={formData.date}
+                onChange={handleDateChange}
+                min={currentDate}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="res-time">Choose time</label>
+              <select
+              className="form-select bg-gray"
+                id="res-time"
+                name="time"
+                value={formData.availableTimes}
+                onChange={handleFormChange}
+                required
+              >
+                {options}
+              </select>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="guests" className="form-label">
+                Number of guests
+              </label>
+              <input
+                type="number"
+                placeholder="1"
+                min="1"
+                max="10"
+                id="guests"
+                name="guests"
+                className="form-control bg-gray"
+                value={formData.guests}
+                onChange={handleFormChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="occasion" className="form-label">
+                Occasion
+              </label>
+              <select
+                id="occasion"
+                name="occasion"
+                className="form-select bg-gray"
+                key={formData.occasion}
+                value={formData.occasion}
+                onChange={handleFormChange}
+                required
+              >
+                <option value="">Select an Option</option>
+                <option>Birthday</option>
+                <option>Anniversary</option>
+              </select>
+            </div>
+            <div>
+              <input
+                aria-label="On Click"
+                type="submit"
+                value={"Make Your Reservation"}
+                className="btn-reserve mx-auto"
+              />
+            </div>
+          </form>
+        </div>
       </div>
     </main>
   );
--- src/pages/BookingConfirmation.jsx
+++ src/pages/BookingConfirmation.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import Button from "../components/Button";
+import { CheckCircle } from "react-bootstrap-icons";
 
 function BookingConfirmation() {
-    return (
-        <>
-        </>
-    )
+  return (
+    <div className="containter">
+      <div className="row">
+        <div className="col-4 mx-auto">
+          <h1 className="mt-4 text-h1 text-center">Your table has been reserved!</h1>
+          <h3 className="h4">You will receive a confirmation email shortly. </h3>
+          <CheckCircle className="confirmation-icon" size={250} color={"#495E57"}/>
+          <Button route={"/"} content={"Return to Home"} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BookingConfirmation;
